b5d01785403b3fe79ae78a5643886f464e1e7ae7
陈乃鑫 <dllo@1208-28.local>
2016-03-29 18:59:32 +0800

    暂存

--- app/src/main/java/com/wuyue/dllo/mirror/activity/WelcomeActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/WelcomeActivity.java
@@ -14,6 +14,7 @@ public class WelcomeActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        
 
 
     }
--- app/src/main/java/com/wuyue/dllo/mirror/activity/activity/base.java
+++
@@ -1,7 +0,0 @@
-package com.wuyue.dllo.mirror.activity.activity;
-
-/**
- * Created by dllo on 16/3/29.
- */
-public class base {
-}
