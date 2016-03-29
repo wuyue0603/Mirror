cda2376c62d9f3227af325ddbc066773dc733556
陈乃鑫 <dllo@1208-28.local>
2016-03-29 17:21:38 +0800

    因为修改包重新拉取

--- app/src/main/java/com/wuyue/dllo/mirror/activity/WelcomeActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/WelcomeActivity.java
@@ -13,5 +13,8 @@ public class WelcomeActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+
     }
 }
