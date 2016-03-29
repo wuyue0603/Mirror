90352608552600a302a8812a77a8b0c2206cf8e4
吴悦 <572512520@qq.com>
2016-03-29 16:57:08 +0800

    欢迎页

--- app/src/main/java/com/wuyue/dllo/mirror/activity/activity/MainActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.wuyue.dllo.mirror;
+package com.wuyue.dllo.mirror.activity.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wuyue.dllo.mirror.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/activity/WelcomeActivity.java
@@ -0,0 +1,7 @@
+package com.wuyue.dllo.mirror.activity.activity;
+
+/**
+ * Created by dllo on 16/3/29.
+ */
+public class WelcomeActivity {
+}
