d93b77809315603ee50e56067dd7ebecc5e660df
张佳奇 <dllo@1208-25zhang.local>
2016-03-30 20:45:00 +0800

    完成了创建账号布局

---
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/CreateAccountNumberActivity.java
@@ -0,0 +1,25 @@
+package com.wuyue.dllo.mirror.activity;
+
+import com.wuyue.dllo.mirror.R;
+import com.wuyue.dllo.mirror.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/3/30.
+ */
+public class CreateAccountNumberActivity extends BaseActivity{
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_create_account_number;
+    }
+}
