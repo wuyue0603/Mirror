5a93f5a1a5d8330286c6ab676f8430d82545fe37
张佳奇 <dllo@1208-25zhang.local>
2016-03-30 19:27:58 +0800

    完成创建账号布局

--- app/src/main/java/com/wuyue/dllo/mirror/activity/MainActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.wuyue.dllo.mirror.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -13,7 +16,7 @@ import com.wuyue.dllo.mirror.R;
 import com.wuyue.dllo.mirror.base.BaseActivity;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
-    TextView titleTv,loginTv;
+    TextView titleTv, loginTv;
     PopupWindow popupWindow;
 
 
@@ -31,9 +34,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void init() {
         titleTv = bindView(R.id.tv);
-        loginTv = bindView(R.id.login_tv);
         titleTv.setOnClickListener(this);
+        loginTv = bindView(R.id.main_login_tv);
         loginTv.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -43,8 +48,10 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private void initView() {
         popupWindow = new PopupWindow(this);
-        final View view = getLayoutInflater().inflate(R.layout.popwindow, null);
+        View view = getLayoutInflater().inflate(R.layout.popwindow, null);
         popupWindow.setContentView(view);
+
+
         popupWindow.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
         popupWindow.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
         popupWindow = new PopupWindow(view, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -52,8 +59,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         popupWindow.setFocusable(true);
         popupWindow.showAsDropDown(view);
         popupWindow.setOutsideTouchable(true);
+        popupWindow.setBackgroundDrawable(null);
+        popupWindow.setAnimationStyle(R.style.PopupAnimation);
+        popupWindow.update();
+
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-        popupWindow.setBackgroundDrawable(getResources().getDrawable(R.mipmap.bg));
 
 
         view.setOnClickListener(new View.OnClickListener() {
@@ -62,12 +72,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             public void onClick(View v) {
                 if (popupWindow.isShowing()) {
                     popupWindow.dismiss();
-                    popupWindow.setAnimationStyle(R.style.PopupAnimation);
-                    popupWindow.update();
-                    popupWindow.dismiss();
+
+//                    popupWindow.update();
+//                    popupWindow.dismiss();
                 }
             }
         });
+
+
     }
 
 
@@ -77,11 +89,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             case R.id.tv:
                 initView();
                 break;
-            case R.id.login_tv:
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            case R.id.main_login_tv:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
                 break;
+
         }
     }
+
+
 }
 
