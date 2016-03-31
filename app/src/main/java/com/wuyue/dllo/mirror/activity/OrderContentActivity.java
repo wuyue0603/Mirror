88959a062cdd73c2f0481a79d6bbcc24a4cf0e1b
吴悦 <572512520@qq.com>
2016-03-31 11:38:58 +0800

    完成订单详情页面布局

---
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/OrderContentActivity.java
@@ -0,0 +1,23 @@
+package com.wuyue.dllo.mirror.activity;
+
+import com.wuyue.dllo.mirror.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/3/31.
+ */
+public class OrderContentActivity extends BaseActivity{
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+}
