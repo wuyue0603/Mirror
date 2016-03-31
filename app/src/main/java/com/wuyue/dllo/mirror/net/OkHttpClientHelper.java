3fd1517216a06033b5dcc9bf317c31e06478a874
张佳奇 <dllo@1208-25zhang.local>
2016-03-31 09:07:29 +0800

    完成发送验证码功能

---
+++ app/src/main/java/com/wuyue/dllo/mirror/net/OkHttpClientHelper.java
@@ -0,0 +1,39 @@
+package com.wuyue.dllo.mirror.net;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by dllo on 16/3/31.
+ */
+public class OkHttpClientHelper {
+    public void getPostDataFromNet(FormEncodingBuilder builder, String leg, final NetLisner netListener) {
+
+        // 创建okHttpClient 对象
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        // 创建一个Request
+        Request request = new Request.Builder().url("http://api101.test.mirroreye.cn/" + leg).post(builder.build()).build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+
+
+            @Override
+            public void onFailure(Request request, IOException e) {
+                netListener.getFaild("失败了");
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String s = response.body().string();
+                netListener.getSucceed(s);
+            }
+        });
+    }
+
+}
