c04eda859a3f594297723967cdb6b92a89510618
吴悦 <572512520@qq.com>
2016-04-07 15:55:39 +0800

    完成首页二级页面滑动和数据拉取

--- app/src/main/java/com/wuyue/dllo/mirror/activity/HomepageContentActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/HomepageContentActivity.java
@@ -1,202 +1,92 @@
 package com.wuyue.dllo.mirror.activity;
 
-import android.content.Context;
-import android.graphics.Rect;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.gson.Gson;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.wuyue.dllo.mirror.R;
 
-import com.wuyue.dllo.mirror.base.BaseActivity;
-import com.wuyue.dllo.mirror.entity.HomepageContentTypeEntity;
+import com.wuyue.dllo.mirror.adapter.DownListViewAdapter;
+import com.wuyue.dllo.mirror.adapter.UpListViewAdapter;
+import com.wuyue.dllo.mirror.entity.AllGoodsListEntity;
+import com.wuyue.dllo.mirror.entity.Costant;
+
+import android.support.v4.view.LinkageListView;
+import android.util.Log;
+
+import java.io.IOException;
 
 /**
  * Created by dllo on 16/3/30.
  */
-public class HomepageContentActivity extends BaseActivity{
-    private RecyclerView recyclerView;
-    private HomepageContentRecyclerViewAdapter adapter;
-    @Override
-    protected void initData() {
-
-    }
-
-    @Override
-    protected int getLayout() {
-        return R.layout.activity_homepagecontent;
-    }
+public class HomepageContentActivity extends Activity {
+    private LinkageListView listView;
+    private AllGoodsListEntity allGoodsListEntity1;
+    private Handler handler;
+    private int position, pos;
+    private SimpleDraweeView background;
+    private String url;
 
     @Override
-    protected void init() {
-        recyclerView = (RecyclerView) findViewById(R.id.homepage_content_recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new HomepageContentRecyclerViewAdapter(this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.addItemDecoration(new MyDecoration());
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepagecontent);
+        listView = (LinkageListView) findViewById(R.id.detail_listview);
+        background = (SimpleDraweeView) findViewById(R.id.goodsdetail_background);
+        allGoodsListEntity1 = new AllGoodsListEntity();
+        Intent intent = getIntent();
+        position = intent.getIntExtra("position", 0);
+        pos = intent.getIntExtra("pos", 0);
+
+
+        post();
+        addData();
     }
-    private class MyDecoration extends RecyclerView.ItemDecoration {
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            super.getItemOffsets(outRect, view, parent, state);
-//            outRect.left = 10;
-//            outRect.right = 10;
-//            outRect.bottom = 10;
-//            outRect.top = 10;
-        }
+
+    private void addData() {
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                Gson gson = new Gson();
+                allGoodsListEntity1 = gson.fromJson(msg.obj.toString(), AllGoodsListEntity.class);
+                Log.d("XXXXXXXXXXXXXX", pos+"handleMessage: ");
+                listView.setAdapter(new UpListViewAdapter(allGoodsListEntity1, getApplication(), pos), new DownListViewAdapter(allGoodsListEntity1, getApplication(), pos));
+                listView.setLinkageSpeed(1.2f);
+                background.setImageURI(Uri.parse(allGoodsListEntity1.getData().getList().get(pos).getGoods_img()));
+                return false;
+            }
+        });
     }
-    public class HomepageContentRecyclerViewAdapter extends RecyclerView.Adapter {
-        private Context context;
-        private HomepageContentTypeEntity entity;
 
-
-        public HomepageContentRecyclerViewAdapter(Context context) {
-            this.context = context;
-            entity = new HomepageContentTypeEntity();
-
-        }
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            RecyclerView.ViewHolder viewHolder = null;
-            switch (viewType) {
-                case HomepageContentTypeEntity.FIRST_TYPE:
-                    View firstView = LayoutInflater.from(context).inflate(R.layout.homepage_content_recyclerview_item_translucent, parent, false);
-                    viewHolder = new ViewHolderFirst(firstView);
-                    break;
-                case HomepageContentTypeEntity.SECOND_TYPE:
-                    View secondView = LayoutInflater.from(context).inflate(R.layout.homepage_content_recyclerview_item_clear, parent, false);
-                    viewHolder = new ViewHolderSecond(secondView);
-                    break;
-                case HomepageContentTypeEntity.THIRD_TYPE:
-                    View thirdView = LayoutInflater.from(context).inflate(R.layout.homepage_content_recyclerview_item_withtitle, parent, false);
-                    viewHolder = new ViewHolderThird(thirdView);
-                    break;
-                case HomepageContentTypeEntity.FOURTH_TYPE:
-                    View fourthView = LayoutInflater.from(context).inflate(R.layout.homepage_content_recyclerview_item_withouttitle, parent, false);
-                    viewHolder = new ViewHolderFourth(fourthView);
-                    break;
+    private void post() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormEncodingBuilder builder = new FormEncodingBuilder();
+        builder.add("device_type", "1");
+                url = Costant.TEXT_HEAD + Costant.GOODS_LIST;
+        Request request = new Request.Builder().url(url).post(builder.build()).build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
             }
-            return viewHolder;
-        }
 
-        public int getItemViewType(int position) {
-            return entity.getType(position);
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            int viewType = getItemViewType(position);
-            switch (viewType) {
-                case HomepageContentTypeEntity.FIRST_TYPE:
-                    ViewHolderFirst viewHolderFirst = (ViewHolderFirst) holder;
-                    //    viewHolderFirst.firstTv.setText(myBean.getData(position));
-                    break;
-
-                case HomepageContentTypeEntity.SECOND_TYPE:
-                    ViewHolderSecond viewHolderSecond = (ViewHolderSecond) holder;
-                    break;
-
-                case HomepageContentTypeEntity.THIRD_TYPE:
-                    ViewHolderThird viewHolderThird = (ViewHolderThird) holder;
-                    viewHolderThird.contentPosition = position;
-                    break;
-                case HomepageContentTypeEntity.FOURTH_TYPE:
-                    ViewHolderFourth viewHolderFourth = (ViewHolderFourth) holder;
-                    viewHolderFourth.contentPosition1 = position;
-                    break;
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String s = response.body().string();
+                Message message = new Message();
+                message.obj = s;
+                handler.sendMessage(message);
             }
-        }
-
-        @Override
-        public int getItemCount() {
-            return entity.getCount();
-        }
-
-        class ViewHolderThird extends RecyclerView.ViewHolder {
-
-            LinearLayout linearLayout;
-            int contentPosition;
-
-            public ViewHolderThird(final View itemView) {
-                super(itemView);
-
-                linearLayout = (LinearLayout) itemView.findViewById(R.id.linearLayout);
-                recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-                    @Override
-                    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                        super.onScrollStateChanged(recyclerView, newState);
-                    }
-
-                    @Override
-                    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                        super.onScrolled(recyclerView, dx, dy);
-                        if (HomepageContentRecyclerViewAdapter.this.getItemViewType(contentPosition) == HomepageContentTypeEntity.THIRD_TYPE) {
-                            linearLayout.scrollTo((int) itemView.getX(), -(int) itemView.getY() / 6);
-                        }
-                    }
-                });
-            }
-        }
-
-        class ViewHolderFourth extends RecyclerView.ViewHolder {
-
-            LinearLayout linearLayout1;
-            int contentPosition1;
-
-            public ViewHolderFourth(final View itemView) {
-                super(itemView);
-
-                linearLayout1 = (LinearLayout) itemView.findViewById(R.id.linearLayout1);
-                recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-                    @Override
-                    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                        super.onScrollStateChanged(recyclerView, newState);
-                    }
-
-                    @Override
-                    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                        super.onScrolled(recyclerView, dx, dy);
-                        if (HomepageContentRecyclerViewAdapter.this.getItemViewType(contentPosition1) == HomepageContentTypeEntity.FOURTH_TYPE) {
-                            linearLayout1.scrollTo((int) itemView.getX(), -(int) itemView.getY() / 6);
-                        }
-                    }
-                });
-            }
-        }
-
-        class ViewHolderFirst extends RecyclerView.ViewHolder {
-            TextView firstTv;
-
-            public ViewHolderFirst(final View itemView) {
-                super(itemView);
-                //   receiveTv = (TextView) itemView.findViewById(R.id.tv_receive);
-            }
-        }
-
-//        class ViewHolderSecond extends RecyclerView.ViewHolder {
-//
-//            TextView secondTv;
-//
-//            public ViewHolderSecond(final View itemView) {
-//                super(itemView);
-//                //   receiveTv = (TextView) itemView.findViewById(R.id.tv_receive);
-//            }
-//        }
-
-
-        class ViewHolderSecond extends RecyclerView.ViewHolder {
-            TextView thirdTv;
-
-            public ViewHolderSecond(final View itemView) {
-                super(itemView);
-                // textView = (TextView) itemView.findViewById(R.id.thirdTextview);
-            }
-        }
+        });
     }
 }
 
---
+++ app/src/main/java/com/wuyue/dllo/mirror/entity/Costant.java
@@ -0,0 +1,11 @@
+package com.wuyue.dllo.mirror.entity;
+
+/**
+ * Created by dllo on 16/4/7.
+ */
+public class Costant {
+    // 测试头
+    public static final String TEXT_HEAD = "http://api101.test.mirroreye.cn/";
+    //商品列表
+    public static final String GOODS_LIST = "index.php/products/goods_list";
+}
