30fe840f62ca1a02ecb2256519a613bacc7a73a6
张佳奇 <dllo@1208-25zhang.local>
2016-04-08 20:40:37 +0800

    完成专题分享二级界面所有功能

--- app/src/main/java/com/wuyue/dllo/mirror/adapter/ThematicSharingAdapter.java
+++ app/src/main/java/com/wuyue/dllo/mirror/adapter/ThematicSharingAdapter.java
@@ -1,6 +1,7 @@
 package com.wuyue.dllo.mirror.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -8,11 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.squareup.picasso.Picasso;
 import com.wuyue.dllo.mirror.R;
+import com.wuyue.dllo.mirror.activity.ThematicSharingActivity;
 import com.wuyue.dllo.mirror.entity.ThematicSharingEntity;
 
 import java.util.List;
@@ -43,7 +46,13 @@ public class ThematicSharingAdapter extends RecyclerView.Adapter<ThematicSharing
     public void onBindViewHolder(MyHolder holder, int position) {
         holder.picIv.setImageURI(Uri.parse(data.getData().getList().get(position).getStory_img()));
         holder.tv.setText(data.getData().getList().get(position).getStory_title());
-        Log.i("43434", data.getData().getList().get(position).getStory_title() + "555");
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ThematicSharingActivity.EnterSecondThme(context,ThematicSharingAdapter.this);
+
+            }
+        });
     }
 
     @Override
@@ -62,11 +71,13 @@ public class ThematicSharingAdapter extends RecyclerView.Adapter<ThematicSharing
     class MyHolder extends RecyclerView.ViewHolder {
         private SimpleDraweeView picIv;
         private TextView tv;
+        private LinearLayout linearLayout;
 
         public MyHolder(View itemView) {
             super(itemView);
             picIv = (SimpleDraweeView) itemView.findViewById(R.id.all_type_iv);
             tv = (TextView) itemView.findViewById(R.id.title);
+            linearLayout = (LinearLayout) itemView.findViewById(R.id.theme_linearLayout);
         }
     }
 }
---
+++ app/src/main/java/com/wuyue/dllo/mirror/fragment/ThematicSharingSecondFragment.java
@@ -0,0 +1,51 @@
+package com.wuyue.dllo.mirror.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wuyue.dllo.mirror.R;
+
+/**
+ * Created by dllo on 16/4/8.
+ */
+public class ThematicSharingSecondFragment extends Fragment{
+    private TextView tv1, tv2,tv3;
+    String s;
+    String title;
+    String subTitle;
+
+    public ThematicSharingSecondFragment(String s, String title, String subTitle) {
+        this.s = s;
+        this.title = title;
+        this.subTitle = subTitle;
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.thematic_sharing_content, container, false);
+        return view;
+    }
+    @Override
+    public void onActivityCreated( Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+    @Override
+    public void onViewCreated(View view,  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tv1 = (TextView) view.findViewById(R.id.theme_smal_title);
+        tv2 = (TextView) view.findViewById(R.id.theme_big_title);
+        tv3 = (TextView) view.findViewById(R.id.theme_content);
+        tv1.setText(s);
+        tv2.setText(title);
+        tv3.setText(subTitle);
+
+
+    }
+}
