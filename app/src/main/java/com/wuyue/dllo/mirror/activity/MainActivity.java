d99051a5ba262b7e423e405369ddd44db0e3e6a5
吴悦 <572512520@qq.com>
2016-04-08 15:42:56 +0800

    删除多余代码

--- app/src/main/java/com/wuyue/dllo/mirror/activity/MainActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/MainActivity.java
@@ -1,36 +1,23 @@
 package com.wuyue.dllo.mirror.activity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.DirectionalViewPager;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.gson.Gson;
 import com.wuyue.dllo.mirror.R;
-import com.wuyue.dllo.mirror.adapter.ShowMenuAdapter;
 import com.wuyue.dllo.mirror.entity.MenuEntity;
-import com.wuyue.dllo.mirror.entity.ShowMenu;
-import com.wuyue.dllo.mirror.entity.GoodsListEntity;
 import com.wuyue.dllo.mirror.fragment.PlaceholderFragment;
 import com.wuyue.dllo.mirror.fragment.ThematicSharingFragment;
 import com.wuyue.dllo.mirror.myinterface.SetTitle;
