cff29d2f5f0ffd383b88956cbd6f1f55e134d585
张佳奇 <dllo@1208-25zhang.local>
2016-04-15 08:53:57 +0800

    张佳奇-完成第三方登录

--- app/src/main/java/com/wuyue/dllo/mirror/activity/LoginActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/LoginActivity.java
@@ -18,6 +18,12 @@ import com.wuyue.dllo.mirror.entity.LoginEntity;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.Callback;
 
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -25,12 +31,13 @@ import okhttp3.Response;
  * Created by dllo on 16/3/29.
  */
 public class LoginActivity extends BaseActivity implements View.OnClickListener {
-    private ImageView closeIv;
+    private ImageView closeIv, sinaLogin;
     private RelativeLayout createNumber;
     private Handler handler;
     private EditText telEt, passwordEt;
     private RelativeLayout relativeLayout;
     private String token;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +58,9 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
         closeIv.setOnClickListener(this);
         createNumber = bindView(R.id.create_number_relative);
 
+
+        sinaLogin = bindView(R.id.sina_login);
+        sinaLogin.setOnClickListener(this);
         telEt = bindView(R.id.login_tel_et);
         passwordEt = bindView(R.id.login_password_et);
         relativeLayout = bindView(R.id.login_relative);
@@ -134,7 +144,35 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
             case R.id.login_close:
                 finish();
                 break;
+            case R.id.sina_login:
+                ShareSDK.initSDK(this);
+                Platform platform = ShareSDK.getPlatform(SinaWeibo.NAME);
+                if (platform.isAuthValid()) {
+                    platform.removeAccount();
+                }
+                platform.setPlatformActionListener(new PlatformActionListener() {
+                    @Override
+                    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+
+                    }
+
+                    @Override
+                    public void onError(Platform platform, int i, Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCancel(Platform platform, int i) {
+
+                    }
+                });
+
+                platform.SSOSetting(false);
+                platform.showUser(null);
+
+                break;
         }
+
     }
 
 }
