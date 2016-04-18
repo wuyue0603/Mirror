59436e371db9afc6a8f9273a43c014ac3eb89f0c
陈乃鑫 <dllo@1208-28.local>
2016-04-18 20:01:04 +0800

    完成支付宝

--- app/src/main/java/com/wuyue/dllo/mirror/activity/OrderContentActivity.java
+++ app/src/main/java/com/wuyue/dllo/mirror/activity/OrderContentActivity.java
@@ -1,22 +1,28 @@
 package com.wuyue.dllo.mirror.activity;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.alipay.sdk.app.PayTask;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.gson.Gson;
 import com.wuyue.dllo.mirror.R;
 
+import com.wuyue.dllo.mirror.alipay.PayResult;
 import com.wuyue.dllo.mirror.base.BaseActivity;
+import com.wuyue.dllo.mirror.entity.AilpayEntity;
 import com.wuyue.dllo.mirror.entity.MyAddressEntity;
 import com.wuyue.dllo.mirror.entity.MyAddressListEntity;
 import com.wuyue.dllo.mirror.net.GetInfo;
@@ -40,6 +46,49 @@ public class OrderContentActivity extends BaseActivity {
     private String price, id;
     private ImageView sureOrderIv;
     private int requesCode = 102;
+    private RelativeLayout relativelayout;
+    private static final int SDK_PAY_FLAG = 1;
+    private String payInfo;
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @SuppressWarnings("unused")
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SDK_PAY_FLAG: {
+                    PayResult payResult = new PayResult((String) msg.obj);
+                    /**
+                     * 同步返回的结果必须放置到服务端进行验证（验证的规则请看https://doc.open.alipay.com/doc2/
+                     * detail.htm?spm=0.0.0.0.xdvAU6&treeId=59&articleId=103665&
+                     * docType=1) 建议商户依赖异步通知
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为“9000”则代表支付成功，具体状态码代表含义可参考接口文档
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        Toast.makeText(OrderContentActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // 判断resultStatus 为非"9000"则代表可能支付失败
+                        // "8000"代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
+                        if (TextUtils.equals(resultStatus, "8000")) {
+                            Toast.makeText(OrderContentActivity.this, "支付结果确认中", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // 其他值就可以判断为支付失败，包括用户主动取消支付，或者系统返回的错误
+                            Toast.makeText(OrderContentActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        };
+    };
+    //private String payInfo;
+
 
     @Override
     protected void initData() {
@@ -49,7 +98,16 @@ public class OrderContentActivity extends BaseActivity {
     @Override
     protected void init() {
         setAddress();
+//        Log.d("chennai", addr_id);
+//        Log.d("chennai", order_id);
+//        Log.d("chennai", good_name);
+
+
+
+
         writeTv = bindView(R.id.write_address);
+        //payInfo = "service=\"mobile.securitypay.pay\"&partner=\"2088021758262531\"&_input_charset=\"utf-8\"&notify_url=\"http%3A%2F%2Fapi.mirroreye.cn%2Findex.php%2Fali_notify\"&out_trade_no=\"1460549569E0z\"&subject=\"\"&payment_type=\"1\"&seller_id=\"2088021758262531\"&total_fee=\"450.00\"&body=\"\"&it_b_pay =\"30m\"&sign=\"gTGWMJaE1llMKQ4PMknIm21GT7JyLGm%2FowrTArlIVmQjoBp7MfX8lx%2BN3UqcyKGLNZnny6kW7L8rsHF9IKFBXFsORRtsgTc7MBS%2Bx4FqcvH%2B25tnkNZIGDhCtUXZT8JFrvVdYLa0Fiy5ufTqMap0ItWroqjwFH71TxucObdCqE8%3D\"&sign_type=\"RSA\"";
+
         writeTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,17 +122,32 @@ public class OrderContentActivity extends BaseActivity {
             }
         });
     }
+
+
+
+    String addr_id;
+    String order_id;
+    String good_name;
     public  void setAddress(){
+
+
         brandTv = bindView(R.id.things_brand);
         priceTv = bindView(R.id.things_price);
         closeIv = bindView(R.id.login_close);
         sureOrderIv = bindView(R.id.sure_list_iv);
-        sureOrderIv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                payDialog();
-            }
-        });
+
+//        sureOrderIv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //payDialog(addr_id,order_id,good_name);//这里有可能为空
+//                AlertDialog.Builder builder = new AlertDialog.Builder(OrderContentActivity.this);
+//                View view = getLayoutInflater().inflate(R.layout.pay_dialog, null);
+//                builder.setView(view);
+//                builder.show();
+//                relativelayout = (RelativeLayout) view.findViewById(R.id.relativelayout);
+//            }
+//        });
+
         receiverInfo = bindView(R.id.receiver_info);
         orderName = bindView(R.id.order_content_name);
         orderTel = bindView(R.id.order_content_tel);
@@ -82,10 +155,34 @@ public class OrderContentActivity extends BaseActivity {
         img = bindView(R.id.things_iv);
 
         handler = new Handler(new Handler.Callback() {
+
             @Override
             public boolean handleMessage(Message msg) {
                 MyAddressEntity entity1 = new Gson().fromJson(msg.obj.toString(), MyAddressEntity.class);
                 if (entity1 != null) {
+
+                    addr_id = entity1.getData().getAddress().getAddr_id();
+                     order_id = entity1.getData().getOrder_id();
+                     good_name = entity1.getData().getGoods().getGoods_name();
+
+
+                    sureOrderIv.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+                            payDialog(addr_id, order_id, good_name);
+                        }
+                    });
+
+
+
+//
+//                    Log.d("chennaiqian",entity1.getData().getAddress().getAddr_id() + " " + "addr_id");
+//                    Log.d("chennaiqian",entity1.getData().getOrder_id() + " " + "order_id");
+//                    Log.d("chennaiqian",entity1.getData().getGoods().getGoods_name() + " " + "good_name");
+
+
+
                     orderName.setText("收件人:" + entity1.getData().getAddress().getUsername());
                     orderTel.setText(entity1.getData().getAddress().getCellphone());
                     orderAddress.setText("地址:" + entity1.getData().getAddress().getAddr_info());
@@ -129,11 +226,95 @@ public class OrderContentActivity extends BaseActivity {
         });
     }
 
-    private void payDialog() {
+    private void payDialog(String addr_id,String order_id,String good_name) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         View view = getLayoutInflater().inflate(R.layout.pay_dialog, null);
         builder.setView(view);
         builder.show();
+        relativelayout = (RelativeLayout) view.findViewById(R.id.relativelayout);
+
+        String url = "http://api101.test.mirroreye.cn/index.php/pay/ali_pay_sub";
+        OkHttpUtils.post().url(url).addParams("token", "433ae165cc754e151c0e8de2ed6ba152").addParams("order_no", order_id).addParams("addr_id", addr_id)
+                .addParams("goodsname",good_name).build().execute(new Callback() {
+            @Override
+            public Object parseNetworkResponse(Response response) throws Exception {
+                String body = response.body().string();
+
+                AilpayEntity ailpayEntity = new Gson().fromJson(body.toString(), AilpayEntity.class);
+                payInfo = ailpayEntity.getData().getStr();
+               // payInfo = "service=\"mobile.securitypay.pay\"&partner=\"2088021758262531\"&_input_charset=\"utf-8\"&notify_url=\"http%3A%2F%2Fapi.mirroreye.cn%2Findex.php%2Fali_notify\"&out_trade_no=\"1460549569E0z\"&subject=\"\"&payment_type=\"1\"&seller_id=\"2088021758262531\"&total_fee=\"450.00\"&body=\"\"&it_b_pay =\"30m\"&sign=\"gTGWMJaE1llMKQ4PMknIm21GT7JyLGm%2FowrTArlIVmQjoBp7MfX8lx%2BN3UqcyKGLNZnny6kW7L8rsHF9IKFBXFsORRtsgTc7MBS%2Bx4FqcvH%2B25tnkNZIGDhCtUXZT8JFrvVdYLa0Fiy5ufTqMap0ItWroqjwFH71TxucObdCqE8%3D\"&sign_type=\"RSA\"";
+
+
+//                Message message = new Message();
+//                message.obj = body;
+//                handler.sendMessage(message);
+                return null;
+            }
+
+            @Override
+            public void onError(Call call, Exception e) {
+
+            }
+
+            @Override
+            public void onResponse(Object response) {
+
+                relativelayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Runnable payRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        // 构造PayTask 对象
+                        PayTask alipay = new PayTask(OrderContentActivity.this);
+                        // 调用支付接口，获取支付结果
+                       // payInfo = "service=\"mobile.securitypay.pay\"&partner=\"2088021758262531\"&_input_charset=\"utf-8\"&notify_url=\"http%3A%2F%2Fapi.mirroreye.cn%2Findex.php%2Fali_notify\"&out_trade_no=\"1460549569E0z\"&subject=\"\"&payment_type=\"1\"&seller_id=\"2088021758262531\"&total_fee=\"450.00\"&body=\"\"&it_b_pay =\"30m\"&sign=\"gTGWMJaE1llMKQ4PMknIm21GT7JyLGm%2FowrTArlIVmQjoBp7MfX8lx%2BN3UqcyKGLNZnny6kW7L8rsHF9IKFBXFsORRtsgTc7MBS%2Bx4FqcvH%2B25tnkNZIGDhCtUXZT8JFrvVdYLa0Fiy5ufTqMap0ItWroqjwFH71TxucObdCqE8%3D\"&sign_type=\"RSA\"";
+
+                        String result = alipay.pay(payInfo, true);
+
+                        Message msg = new Message();
+                        msg.what = SDK_PAY_FLAG;
+                        msg.obj = result;
+                        mHandler.sendMessage(msg);
+                    }
+                };
+
+                // 必须异步调用
+                Thread payThread = new Thread(payRunnable);
+                payThread.start();
+            }
+        });
+
+            }
+        });
+
+
+//        relativelayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Runnable payRunnable = new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        // 构造PayTask 对象
+//                        PayTask alipay = new PayTask(OrderContentActivity.this);
+//                        // 调用支付接口，获取支付结果
+//                        String result = alipay.pay(payInfo, true);
+//
+//                        Message msg = new Message();
+//                        msg.what = SDK_PAY_FLAG;
+//                        msg.obj = result;
+//                        mHandler.sendMessage(msg);
+//                    }
+//                };
+//
+//                // 必须异步调用
+//                Thread payThread = new Thread(payRunnable);
+//                payThread.start();
+//            }
+//        });
+
+
+
     }
 
     @Override
